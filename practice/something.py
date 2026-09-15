6a6536d0242cfdf0ea6a23873ddf96f713a60563
Landon Johnson <lanman100102@gmail.com>
2026-09-14 19:07:22 -0500

    Creating a simple python script that asks for my name and how my day is going, will turn into somehting else sooner or later.

---
+++ practice/something.py
@@ -0,0 +1,9 @@
+name = input("Please tell me your name: ")
+
+
+if name == "Landon":
+	print("Greetings sir")
+	birth_day = input("Please tell me your birthday: ")
+	print(birth_day)
+else:
+	print("Not valid")
