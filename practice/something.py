6dce15e55a98f964ad49915f23a1b2525b254e87
Landon Johnson <lanman100102@gmail.com>
2026-09-15 15:25:52 -0500

    Beginning parts of my code. still have to include different api's to have it link to music applications or youtube to play certain thigns for me. THe point is to make it as seemless as possible to ensure the safety of my mental health.

--- practice/something.py
+++
@@ -1,9 +0,0 @@
-name = input("Please tell me your name: ")
-
-
-if name == "Landon":
-	print("Greetings sir")
-	birth_day = input("Please tell me your birthday: ")
-	print(birth_day)
-else:
-	print("Not valid")
